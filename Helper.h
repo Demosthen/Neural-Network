a4594765ee8b090e401d1451a99796cee97d7945
Demosthen <1008981@gmail.com>
2019-09-10 14:47:29 -0700

    Clean up code

--- Helper.h
+++ Helper.h
@@ -6,7 +6,6 @@ class Helper
 public:
 	Helper();
 	static bool inline checkBounds(shared_ptr<ppp> bounds, pair<double, double> pos){
-		//cout << pos.first << " " << pos.second << "? " << bounds->first << " " << bounds->second.first << " " << bounds->second.second.first << " " << bounds->second.second.second << endl;
 		return bounds->first <= pos.first&&bounds->second.first <= pos.second&&bounds->second.second.first > pos.first&&bounds->second.second.second>pos.second;
 	}
 	~Helper();
